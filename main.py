3d0f66662efb1fada04d1a64fa209d36616d1050
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-06-23 12:55:11 +0530

    plotting a scatter plot

--- main.py
+++ main.py
@@ -62,3 +62,14 @@ plt.xlabel('age')
 plt.ylabel('count')
 plt.title('Age Histogram')
 plt.show()
+
+# plotting a scatter plot
+
+x = [1,2,3,4,5,6,7,8]
+y = [5,2,4,2,1,4,5,2]
+plt.scatter(x,y,label = 'skitscat', color = 'k', marker = 'o', s=25)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Scatter Plot')
+plt.legend()
+plt.show()
