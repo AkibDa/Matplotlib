076146da769edbd20b42484e4215cc06828af3bf
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-06-23 13:00:45 +0530

    plotting a stack plot

--- main.py
+++ main.py
@@ -73,3 +73,25 @@ plt.ylabel('y')
 plt.title('Scatter Plot')
 plt.legend()
 plt.show()
+
+# plotting a stack plot
+
+days = [1,2,3,4,5]
+
+sleeping = [7,8,6,11,7]
+eating = [2,3,4,3,2]
+working = [7,8,7,2,2]
+playing = [8,5,7,8,13]
+
+plt.plot([],[],color = 'm',label = 'Sleeping', linewidth = 5)
+plt.plot([],[],color = 'c',label = 'Eating', linewidth = 5)
+plt.plot([],[],color = 'r',label = 'Working', linewidth = 5)
+plt.plot([],[],color = 'k',label = 'Playing', linewidth = 5)
+
+plt.stackplot(days,sleeping,eating,working,playing)
+
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Stack Plot')
+plt.legend()
+plt.show()
